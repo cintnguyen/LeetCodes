29d98a663a8f52dd3701d24c86e804087543175c
cintnguyen <cintnguyen98@gmail.com>
2024-03-04 23:52:29 -0500

    leetcode 122

--- medium/122 Best Time to Buy and Sell Stock II.js
+++ medium/122 Best Time to Buy and Sell Stock II.js
@@ -60,3 +60,13 @@ var maxProfit = function(prices) {
     }
     return total
 };
+
+function maxProfit(prices) {
+    let profit = 0
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] - prices[i-1] > 0) {
+            profit += prices[i] - prices[i-1]
+        }
+    }
+    return profit
+}
